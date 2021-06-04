41ddf41b9fc65b017ba20e40c73b8a222dabbaf7
liubenben <liubenben@liubenbendeMacBook-Pro.local>
2021-06-04 15:18:52 +0800

    20210604刷题:回文链表

--- question_234.py
+++ question_234.py
@@ -21,10 +21,10 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def reverse(self, head):
         result = None
