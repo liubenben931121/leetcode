af1b750741184f1e530c6ec902ac7ea2428b139b
liubenben <liubenben@liubenbendeMacBook-Pro.local>
2021-07-07 20:37:10 +0800

    20210707日刷题

---
+++ question_82.py
@@ -0,0 +1,58 @@
+# 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+#
+#  返回同样按升序排列的结果链表。
+#
+#
+#
+#  示例 1：
+#
+#
+# 输入：head = [1,2,3,3,4,4,5]
+# 输出：[1,2,5]
+#
+#
+#  示例 2：
+#
+#
+# 输入：head = [1,1,1,2,3]
+# 输出：[2,3]
+#
+#
+#
+#
+#  提示：
+#
+#
+#  链表中节点数目在范围 [0, 300] 内
+#  -100 <= Node.val <= 100
+#  题目数据保证链表已经按升序排列
+#
+#  Related Topics 链表 双指针
+#  👍 652 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        phead = ListNode(-101, head)
+        p = phead
+        value = p.val
+        while p.next:
+            if value == p.next.val:
+                p.next = p.next.next
+                continue
+            if p.next.next:
+                if p.next.val == p.next.next.val:
+                    value = p.next.val
+                    continue
+                else:
+                    value = -101
+            p = p.next
+
+        return phead.next
+# leetcode submit region end(Prohibit modification and deletion)
