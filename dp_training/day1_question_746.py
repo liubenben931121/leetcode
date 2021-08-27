bc0e715e7e75c2517efaeaa44ecaca4ed85d4595
liubenben <liubenben@example.com>
2021-08-27 14:02:28 +0800

    20210827刷题

---
+++ dp_training/day1_question_746.py
@@ -0,0 +1,35 @@
+"""
+746. 使用最小花费爬楼梯
+数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的体力花费值 cost[i]（下标从 0 开始）。
+
+每当你爬上一个阶梯你都要花费对应的体力值，一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。
+
+请你找出达到楼层顶部的最低花费。在开始时，你可以选择从下标为 0 或 1 的元素作为初始阶梯。
+
+
+
+示例 1：
+
+输入：cost = [10, 15, 20]
+输出：15
+解释：最低花费是从 cost[1] 开始，然后走两步即可到阶梯顶，一共花费 15 。
+ 示例 2：
+
+输入：cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+输出：6
+解释：最低花费方式是从 cost[0] 开始，逐个经过那些 1 ，跳过 cost[3] ，一共花费 6 。
+"""
+class Solution:
+    def minCost(self, cost, result, index):
+        if len(result) > index:
+            return result[index]
+        result.append(min(self.minCost(cost, result, index - 1) , self.minCost(cost,result, index - 2)) + cost[index])
+        return result[index]
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        costs = []
+        costs.append(cost[0])
+        costs.append(cost[1])
+        len_n = len(cost)
+        result = min(self.minCost(cost, costs, len_n-1), self.minCost(cost, costs, len_n-2))
+        return result
