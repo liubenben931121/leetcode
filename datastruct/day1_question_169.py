8f409fab18b66121bd056f88185de7f95666b7b0
liubenben <liubenben@example.com>
2021-08-28 19:03:18 +0800

    20210828刷题

---
+++ datastruct/day1_question_169.py
@@ -0,0 +1,38 @@
+"""
+169. 多数元素
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+
+
+示例 1：
+
+输入：[3,2,3]
+输出：3
+示例 2：
+
+输入：[2,2,1,1,1,2,2]
+输出：2
+
+
+进阶：
+
+尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。
+"""
+
+
+class Solution:
+
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        target = None
+        for item in nums:
+            if count == 0:
+                target = item
+
+            if target == item:
+                count += 1
+            else:
+                count -= 1
+        return target
